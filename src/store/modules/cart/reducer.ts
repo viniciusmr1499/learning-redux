35ce75000ff9c219e1bdff21e0d7c04c67944f9e
Marcos Vinicius <viniciusmr1499@gmail.com>
2021-02-17 21:53:04 -0300

    adicionando componente Cart

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,30 +1,37 @@
 import { Reducer } from "redux";
 import { ICartState } from './types';
+import produce from 'immer';
 
 const INITIAL_STATE: ICartState = {
     items: [],
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        case 'ADD_PRODUCT_TO_CART': {
-            const { product } = action.payload;
-            
-            return {
-                ...state,
-                items: [
-                    ...state.items,
-                    {
+    return produce(state, draft => {
+        switch(action.type) {
+            case 'ADD_PRODUCT_TO_CART': {
+                const { product } = action.payload;
+
+                const productInCartIndex = draft.items.findIndex(item => 
+                    item.product.id === product.id,
+                );
+
+                if(productInCartIndex >= 0) {
+                    draft.items[productInCartIndex].quantity ++;
+                } else {
+                    draft.items.push({
                         product,
                         quantity: 1,
-                    }
-                ]
-            };
+                    });
+                }
+
+                break;
+            }
+            default: {
+                return draft;
+            }
         }
-        default: {
-            return state;
-        }
-    }
+    });
 };
 
 export default cart;
