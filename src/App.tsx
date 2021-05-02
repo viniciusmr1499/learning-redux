5e9831564cd57aea84f44f7ff723c9b11b4e8189
Marcos  Oliveira <viniciusmr1499@gmail.com>
2021-05-02 13:45:24 -0300

    adicionado saga na aplicaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Provider } from 'react-redux';
 import Catalog from './components/Catalog';
 import Cart from './components/Cart';
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,24 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './modules/rootReducer';
-
+import CreateSagaMiddleware from 'redux-saga';
 import { ICartState } from './modules/cart/types';
 
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 export interface IState {
     cart: ICartState;
 }
 
+const sagaMiddleware = CreateSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
 const store = createStore(
    rootReducer,
-   composeWithDevTools(),
+   composeWithDevTools(
+    applyMiddleware(...middlewares)
+   ),
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
